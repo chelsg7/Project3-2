eb0cdf379ce0df76d5887dd0ffe9af6a3d21de31
thorc <chelsgieck@gmail.com>
2018-08-01 02:26:33 -0400

    yarn lock

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { BrowserRouter } from 'react-router-dom' //may help sessions store cooki
 //so that you don't need to specify this in every axios request
 
 //style
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 
 import 'spectre.css/dist/spectre.min.css';
 import 'spectre.css/dist/spectre-icons.css';
@@ -23,6 +23,6 @@ import './index.css';
 ReactDOM.render(
 	<BrowserRouter>
 		<App />
-	</BrowserRouter>,
-	document.getElementById('root')
-)
+</BrowserRouter>,
+	document.getElementById("root")
+);
